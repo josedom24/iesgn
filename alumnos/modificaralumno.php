2807fa6e99bc47c27f0b254463ba0a785275b2da
josedom24 <josedom24@gmail.com>
2012-05-08 00:24:25 +0200

    Se puede modificar nombre y DNI

--- alumnos/modificaralumno.php
+++ alumnos/modificaralumno.php
@@ -12,7 +12,7 @@ if($_POST)
 	
 	if($cont_error==0)
 	{
-		$sql="UPDATE Alumnos SET Unidad='".$_POST['uni']."',Direccion='".$_POST["dir"]."',Localidad='".$_POST["loc"]."',Provincia='".$_POST["pro"]."',CodPostal='".$_POST['cp']."',Ap1tutor='".$_POST['t1']."',Ap2tutor='".$_POST['t2']."',Nomtutor='".$_POST['t0']."' WHERE Id='".$_POST['Id']."'";
+		$sql="UPDATE Alumnos SET Nombre='".$_POST['nom']."',DNI='".$_POST["dni"]."',Unidad='".$_POST['uni']."',Direccion='".$_POST["dir"]."',Localidad='".$_POST["loc"]."',Provincia='".$_POST["pro"]."',CodPostal='".$_POST['cp']."',Ap1tutor='".$_POST['t1']."',Ap2tutor='".$_POST['t2']."',Nomtutor='".$_POST['t0']."' WHERE Id='".$_POST['Id']."'";
 		mysql_query($sql) or die("Error en SQL:".$sql."<br>".mysql_error($bd));		
 		mysql_close();
 		header("Location:alumnos.php");
@@ -35,6 +35,8 @@ permisos("alumnos","E",1);
 		
 		if ($row=mysql_fetch_array($result))
 		{
+			$valor_nom=$row['Nombre'];
+			$valor_dni=$row['DNI'];
 			$valor_unidad=$row['Unidad'];
 			$valor_usuario_modificar=$row['Id'];
 			$valor_dir=$row["Direccion"];
@@ -50,6 +52,8 @@ permisos("alumnos","E",1);
 	}
 	else
 	{
+			$valor_nom=$row['nom'];
+			$valor_dni=$row['dni'];
 			$valor_unidad=$_POST['uni'];
 			$valor_usuario_modificar=$_POST["usuario_a_modificar"];
 			$valor_dir=$_POST["dir"];
@@ -75,7 +79,8 @@ permisos("alumnos","E",1);
 	$sql="select Unidad from Alumnos group by Unidad";
 	$result=mysql_query($sql);
 
-	echo "<br><center>";
+	echo "<br><center><table>";
+	echo "<tr><td>";
 	echo "<select  name=\"uni\" size=\"0\">";
 
 	while($row=mysql_fetch_array($result))
@@ -89,17 +94,48 @@ permisos("alumnos","E",1);
 			echo " value=\"".$row["Unidad"]."\">".$row["Unidad"]."</option>\n";
 	}
 	echo "</select>\n";
-	echo "<br/>";
-	echo "<br/>";
+	echo "</td></tr>";
 
-	echo "Direccion:";
-	echo '<input type="text" name="dir" value="'.$valor_dir.'"/><br/>';
-	echo 'Localidad:<input type="text" name="loc" value="'.$valor_loc.'"/><br/>';
-	echo 'Provincia:<input type="text" name="pro" value="'.$valor_prov.'"/><br/>';
-	echo 'CP:<input type="text" name="cp" value="'.$valor_cp.'"/><br/>';
-	echo 'Nombre Tutor:<input type="text" name="t0" value="'.$valor_t0.'"/><br/>';
-	echo 'Apellido 1 Tutor:<input type="text" name="t1" value="'.$valor_t1.'"/><br/>';
-	echo 'Apellido 2 Tutor:<input type="text" name="t2" value="'.$valor_t2.'"/><br/>';
+	echo "<tr>";
+	echo "<td>Nombre:";
+	echo '<input type="text" name="nom" value="'.$valor_nom.'" size="50"/></td>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>DNI:";
+	echo '<input type="text" name="dni" value="'.$valor_dni.'"/></td>';
+	echo "</tr>";
+
+
+	echo "<tr>";
+	echo "<td>Dirección:";
+	echo '<input type="text" name="dir" value="'.$valor_dir.'" size="50"/></td>';
+	echo "</tr>";
+	
+	echo "<tr>";
+	echo '<td>Localidad:<input type="text" name="loc" value="'.$valor_loc.'"/></td>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>Provincia:<input type="text" name="pro" value="'.$valor_prov.'"/></td>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>CP:<input type="text" name="cp" value="'.$valor_cp.'"/><td/>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>Nombre Tutor:<input type="text" name="t0" value="'.$valor_t0.'"/></td>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>Apellido 1 Tutor:<input type="text" name="t1" value="'.$valor_t1.'"/></td>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>Apellido 2 Tutor:<input type="text" name="t2" value="'.$valor_t2.'"/></td>';
+	echo "</tr>";
+	echo "</table>";
 
 
 	?>
