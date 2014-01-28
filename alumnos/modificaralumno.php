5a1c2b7c647cd3459b2c59905cf98fc5674e12a0
root <root@informatica.gonzalonazareno.org>
2014-01-28 13:28:36 +0100

    campos tel1,tel2 y observaciones

--- alumnos/modificaralumno.php
+++ alumnos/modificaralumno.php
@@ -12,7 +12,7 @@ if($_POST)
 	
 	if($cont_error==0)
 	{
-		$sql="UPDATE Alumnos SET Nombre='".$_POST['nom']."',DNI='".$_POST["dni"]."',Unidad='".$_POST['uni']."',Direccion='".$_POST["dir"]."',Localidad='".$_POST["loc"]."',Provincia='".$_POST["pro"]."',CodPostal='".$_POST['cp']."',Ap1tutor='".$_POST['t1']."',Ap2tutor='".$_POST['t2']."',Nomtutor='".$_POST['t0']."' WHERE Id='".$_POST['Id']."'";
+		$sql="UPDATE Alumnos SET Nombre='".$_POST['nom']."',DNI='".$_POST["dni"]."',Unidad='".$_POST['uni']."',Direccion='".$_POST["dir"]."',Localidad='".$_POST["loc"]."',Provincia='".$_POST["pro"]."',CodPostal='".$_POST['cp']."',Ap1tutor='".$_POST['t1']."',Ap2tutor='".$_POST['t2']."',Nomtutor='".$_POST['t0']."',Telefono1='".$_POST["tel1"]."',Telefono2='".$_POST["tel2"]."',Obs='".$_POST["obs"]."' WHERE Id='".$_POST['Id']."'";
 		mysql_query($sql) or die("Error en SQL:".$sql."<br>".mysql_error($bd));		
 		mysql_close();
 		header("Location:alumnos.php");
@@ -46,6 +46,9 @@ permisos("alumnos","E",1);
 			$valor_t0=$row["Nomtutor"];			
 			$valor_t1=$row["Ap1tutor"];			
 			$valor_t2=$row["Ap2tutor"];			
+			$valor_tel1=$row["Telefono1"];			
+			$valor_tel2=$row["Telefono2"];			
+			$valor_obs=$row["Obs"];			
 
 		}
 		
@@ -63,6 +66,10 @@ permisos("alumnos","E",1);
 			$valor_t0=$_POST["t0"];			
 			$valor_t1=$_POST["t1"];			
 			$valor_t2=$_POST["t2"];			
+			$valor_tel1=$_POST["tel1"];			
+			$valor_tel2=$_POST["tel2"];			
+			$valor_obs=$_POST["obs"];			
+
 
 
 			
@@ -135,6 +142,23 @@ permisos("alumnos","E",1);
 	echo "<tr>";
 	echo '<td>Apellido 2 Tutor:<input type="text" name="t2" value="'.$valor_t2.'"/></td>';
 	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>Teléfono 1:<input type="text" name="tel1" value="'.$valor_tel1.'"/></td>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>Teléfono 2:<input type="text" name="tel2" value="'.$valor_tel2.'"/></td>';
+	echo "</tr>";
+
+	echo "<tr>";
+	echo '<td>Observaciones:</td>';
+	echo "</tr>";
+	echo "<tr>";
+	echo '<td><textarea name="obs" rows="15" cols="80">'.$valor_obs.'</textarea>';
+	echo "</tr>";
+
+	
 	echo "</table>";
 
 
@@ -149,3 +173,4 @@ permisos("alumnos","E",1);
 }include ($_SERVER["DOCUMENT_ROOT"]."/iesgn/includes/footer.inc");
 
 ?>
+ 
